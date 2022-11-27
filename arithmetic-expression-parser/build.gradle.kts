ff5d71b69b54b61e7f1574e7e6a9583f34632cec
Julian Bühler <juli27@users.noreply.github.com>
2022-11-27 15:57:43 +0100

    Update Gradle wrapper from 7.4 to 7.6

--- arithmetic-expression-parser/build.gradle.kts
+++ arithmetic-expression-parser/build.gradle.kts
@@ -1,18 +1,24 @@
 plugins {
-    id("java")
+    application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
-dependencies {
-    //testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    //testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-}
+//repositories {
+//    mavenCentral()
+//}
+
+//dependencies {
+//    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+//    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+//}
 
 //tasks.getByName<Test>("test") {
 //    useJUnitPlatform()
