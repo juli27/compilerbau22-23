8659303791026bc10f14e59a909fdb9315d1a3c3
Julian Bühler <699919+juli27@users.noreply.github.com>
2022-12-26 10:35:33 +0100

    build: Switch to JDK 19 with preview features

--- arithmetic-expression-parser/build.gradle.kts
+++ arithmetic-expression-parser/build.gradle.kts
@@ -7,8 +7,18 @@ version = "1.0-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion.set(JavaLanguageVersion.of(19))
     }
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("--enable-preview")
+}
+tasks.withType<Test> {
+    jvmArgs("--enable-preview")
+}
+tasks.withType<JavaExec> {
+    jvmArgs("--enable-preview")
 }
 
 //repositories {
